50e9d57a53d85b73c6a5a9e516e84367f6097896
Aliendreamer <dreamingman83@gmail.com>
2024-12-01 18:02:59 +0200

    .

--- adventofcode2024/1.js
+++ adventofcode2024/1.js
@@ -52,7 +52,22 @@ const handleInput = ()=>{
         return { list1: array1, list2: array2};
 }
 
-const inputs = handleInput();
-console.log(task2(inputs.list1,inputs.list2));
+// const inputs = handleInput();
+// console.log(task1(inputs.list1,inputs.list2));
 
 // console.log(task2([3,4,2,1,3,3],[4,3,5,3,9,3]));
+
+const task1Oneline  = () => {
+    return fs.readFileSync(path.resolve(
+        "adventofcode2024",'1input.txt'), 'utf8').split('\n')
+        .reduce((acc, curr) => {
+            const nums = curr.split(' ').filter(Boolean).map(Number);
+            acc[0].push(nums[0]);
+            acc[1].push(nums[1]);
+            return acc;
+        },  [ [],  []])
+        .map((x) => x.sort((a,b)=>a-b))
+        .reduce((acc,curr,index) =>(index===0? acc =[...curr]: acc = acc.map((x,i)=>Math.abs(x-curr[i]))),[])
+        .reduce((sum, diff) => sum + diff, 0);
+}
+console.log(task1Oneline());
