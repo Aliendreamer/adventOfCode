aea076bd3aa2be27ee6053e3d3ab8cd9aaa9e12e
Teodor Stefanov <teodor.g.stefanov@a1.bg>
2024-12-12 18:06:01 +0200

    yes

--- adventofcode2024/day12/12.js
+++ adventofcode2024/day12/12.js
@@ -63,60 +63,43 @@ const task1 = () => {
     let elapsed_time = ftime - stime;
     console.log(`Execution time task1: ${elapsed_time} ms`);
 };
-// task1();
 
-// (data, row, col) => data[row][col] === regionId
-function dfs(data, row, col, visited, isMoveAllowed, onMove) {
-    onMove(data, row, col, visited);
-    visited.add(`${row},${col}`);
-    const rols = data.length;
-    const cols = data[0].length;
-    GridMovePatterns.forEach(([dx, dy]) => {
-        const [nextRow, nextCol] = [row + dx, col + dy];
-        if (
-            !visited.has(`${nextRow},${nextCol}`) &&
-            isValid(nextRow, nextCol, cols, rols) &&
-            isMoveAllowed(data, nextRow, nextCol, visited)
-        ) {
-            dfs(data, nextRow, nextCol, visited, isMoveAllowed, onMove);
-        }
-    });
-}
+const bfsRecursive2 = (gardens, start, visited) => {
+    const rows = gardens.length;
+    const cols = gardens[0].length;
+    const queue = [start];
+    const gardenPlots = new Set();
+    const gardenFences = new Map();
+    while (queue.length > 0) {
+        const [r, c] = queue.shift();
+        const key = `${r},${c}`;
+        if (visited.has(key)) continue;
+        if (gardenPlots.has(key)) continue;
+        gardenPlots.add(key);
+        visited.set(key);
 
-function measure(data, row, col, visited) {
-    const regionId = data[row][col];
-    let perimeter = 0;
-    let region = 0;
-    const dirToFences = new Map();
-
-    dfs(
-        data,
-        row,
-        col,
-        visited,
-        (data, row, col) => data[row][col] === regionId,
-        (data, row, col, visited) => {
-            if (visited.has(`${row},${col}`)) {
-                return;
+        // Explore neighbors with a height increase of exactly 1
+        for (const [dr, dc] of GridMovePatterns) {
+            const nr = r + dr;
+            const nc = c + dc;
+            if (isValid(nr, nc, rows, cols) && !visited.has(`${nr},${nc}`) && canContinue(gardens[r][c], gardens[nr][nc])) {
+                queue.push([nr, nc]);
             }
-            if (data[row][col] === regionId) {
-                region++;
-            }
-            GridMovePatterns.forEach(([dx, dy]) => {
-                if (data[row + dx]?.[col + dy] !== regionId) {
-                    perimeter++;
-
-                    const dirHash = `${dx},${dy}`;
-                    if (!dirToFences.has(dirHash)) {
-                        dirToFences.set(dirHash, []);
-                    }
-                    dirToFences.get(dirHash).push([row + dx, col + dy]);
+            if (!canContinue(gardens?.[r]?.[c], gardens?.[nr]?.[nc])) {
+                const dirHash = `${dr},${dc}`;
+                if (!gardenFences.has(dirHash)) {
+                    gardenFences.set(dirHash, []);
                 }
-            });
+                gardenFences.get(dirHash).push([nr, nc]);
+            }
         }
-    );
+    }
 
-    const sides = [...dirToFences.entries()].reduce((sides, [hash, fences]) => {
+    for (const element of gardenPlots.values()) {
+        const [r, c] = element.split(',').map(Number);
+        gardens[r][c] = 0;
+    }
+    const sides = [...gardenFences.entries()].reduce((sides, [hash, fences]) => {
         const affectsRow = !hash.startsWith('0,');
         let localSides = 1;
 
@@ -140,11 +123,8 @@ function measure(data, row, col, visited) {
         return sides + localSides;
     }, 0);
 
-    return {
-        region,
-        sides,
-    };
-}
+    return gardenPlots.size * sides;
+};
 
 const task2 = () => {
     let stime = performance.now();
@@ -153,20 +133,21 @@ const task2 = () => {
         .trim()
         .split('\n')
         .map((x) => x.split(''));
-    const visited = new Set();
-    let bulkDiscountCost = 0;
-
-    for (let row = 0; row < gardens.length; ++row) {
-        for (let col = 0; col < gardens[0].length; ++col) {
-            const { region, sides } = measure(gardens, row, col, visited);
-            bulkDiscountCost += region * sides;
-        }
-    }
-
-    console.log(bulkDiscountCost);
+    const map = new Map();
+    const result = gardens
+        .map((x, index) =>
+            x.map((y, i) => {
+                return bfsRecursive2(gardens, [index, i], map);
+            })
+        )
+        .flatMap((x) => x)
+        .filter((x) => x != 0)
+        .reduce((a, b) => a + b, 0);
+    console.log(result);
     let ftime = performance.now();
     let elapsed_time = ftime - stime;
-    console.log(`Execution time task2: ${elapsed_time} ms`);
+    console.log(`Execution time task1: ${elapsed_time} ms`);
 };
 
+task1();
 task2();
