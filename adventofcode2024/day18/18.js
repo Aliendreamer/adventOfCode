3e9f856067b1e591d79004866e7895dbc24fe7d8
Teodor Stefanov <teodor.g.stefanov@a1.bg>
2024-12-18 19:46:54 +0200

    both parts done

--- adventofcode2024/day18/18.js
+++ adventofcode2024/day18/18.js
@@ -3,63 +3,14 @@ import { readInput, measurementWrapper, isValid, GridMovePatterns, drawGrid } fr
 
 const isWall = (position) => position === '#';
 const toKey = (r, c, dir) => `${r},${c},${dir}`;
+
 const task1 = () => {
-    const filepath = path.resolve(process.cwd(), 'adventofcode2024', 'day18', 'input.txt');
-    const positions = readInput(filepath)
-        .split('\n')
-        .map((x) => x.split(',').map(Number))
-        .slice(0, 1024);
-    const grid = Array.from({ length: 71 }, () => Array.from({ length: 71 }, () => '.'));
-    positions.forEach(([x, y]) => {
-        grid[y][x] = '#';
-    });
-    drawGrid(grid);
-    const rows = grid.length;
-    const cols = grid[0].length;
-    const start = [0, 0];
-    const end = [rows - 1, cols - 1];
-    const visited = new Set();
-    const distances = new Map();
-    let sum = Infinity;
-    const heap = [];
-    heap.push([start[0], start[1], undefined, 0]);
-    while (heap.length > 0) {
-        const [r, c, dir, steps] = heap.shift();
-
-        const currentKey = toKey(r, c, dir);
-        if (visited.has(currentKey)) {
-            continue;
-        }
-        visited.add(currentKey);
-        // If we reach the end, return the score
-        if (r === end[0] && c === end[1]) {
-            sum = sum > steps ? steps : sum;
-        }
-
-        for (let index = 0; index < GridMovePatterns.length; index++) {
-            const element = GridMovePatterns[index];
-            const nr = r + element[0];
-            const nc = c + element[1];
-            if (isValid(nr, nc, rows, cols) && !isWall(grid[nr][nc])) {
-                const key = toKey(nr, nc, index);
-                if (visited.has(key)) {
-                    continue;
-                }
-                distances.set(key, steps); // i may not need distances
-                heap.push([nr, nc, index, steps + 1]);
-            }
-        }
-    }
-    console.log(sum);
-};
-
-const task1a = () => {
     const filepath = path.resolve(process.cwd(), 'adventofcode2024', 'day18', 'input.txt');
     const input = readInput(filepath)
         .split('\n')
         .map((x) => x.split(',').map(Number));
-    const positions = input.slice(0, 12);
-    const grid = Array.from({ length: 7 }, () => Array.from({ length: 7 }, () => '.'));
+    const positions = input.slice(0, 1024);
+    const grid = Array.from({ length: 71 }, () => Array.from({ length: 71 }, () => '.'));
     positions.forEach(([x, y]) => {
         grid[y][x] = '#';
     });
@@ -81,7 +32,7 @@ const task1a = () => {
             continue;
         }
         visited.add(currentKey);
-        // If we reach the end, return the score
+
         if (r === end[0] && c === end[1]) {
             if (sum > steps) {
                 sum = steps;
@@ -111,9 +62,9 @@ const task2 = () => {
     const input = readInput(filepath)
         .split('\n')
         .map((x) => x.split(',').map(Number));
-    const positions = input.slice(0, 12);
-    const corruptedBytes = input.slice(12);
-    const grid = Array.from({ length: 7 }, () => Array.from({ length: 7 }, () => '.'));
+    const positions = input.slice(0, 1024);
+    const corruptedBytes = input.slice(1024);
+    const grid = Array.from({ length: 71 }, () => Array.from({ length: 71 }, () => '.'));
     positions.forEach(([x, y]) => {
         grid[y][x] = '#';
     });
@@ -121,41 +72,48 @@ const task2 = () => {
     const cols = grid[0].length;
     const start = [0, 0];
     const end = [rows - 1, cols - 1];
-    const visited = new Set();
-    let sum = Infinity;
-    const heap = [];
-    heap.push([start[0], start[1], undefined, 0]);
-    while (heap.length > 0) {
-        const [r, c, dir, steps] = heap.shift();
+    let coords = [];
+    for (const [x, y] of corruptedBytes) {
+        const visited = new Set();
+        grid[y][x] = '#';
+        let ended = false;
+        const heap = [];
+        coords = [x, y];
+        heap.push([start[0], start[1], undefined]);
+        while (heap.length > 0) {
+            const [r, c, dir] = heap.shift();
 
-        const currentKey = toKey(r, c, dir);
-        if (visited.has(currentKey)) {
-            continue;
-        }
-        visited.add(currentKey);
-        // If we reach the end, return the score
-        if (r === end[0] && c === end[1]) {
-            if (sum > steps) {
-                sum = steps;
+            const currentKey = toKey(r, c, dir);
+            if (visited.has(currentKey)) {
+                continue;
             }
-        }
+            visited.add(currentKey);
+            // If we reach the end, return the score
+            if (r === end[0] && c === end[1]) {
+                ended = true;
+                break;
+            }
 
-        for (let index = 0; index < GridMovePatterns.length; index++) {
-            const element = GridMovePatterns[index];
-            const nr = r + element[0];
-            const nc = c + element[1];
-            if (isValid(nr, nc, rows, cols) && !isWall(grid[nr][nc])) {
-                const key = toKey(nr, nc, index);
-                if (visited.has(key)) {
-                    continue;
+            for (let index = 0; index < GridMovePatterns.length; index++) {
+                const element = GridMovePatterns[index];
+                const nr = r + element[0];
+                const nc = c + element[1];
+                if (isValid(nr, nc, rows, cols) && !isWall(grid[nr][nc])) {
+                    const key = toKey(nr, nc, index);
+                    if (visited.has(key)) {
+                        continue;
+                    }
+                    heap.push([nr, nc, index]);
                 }
-                heap.push([nr, nc, index, steps + 1]);
             }
+        }
+        if (!ended) {
+            break;
         }
     }
 
-    console.log(sum);
+    console.log(coords.join(','));
 };
 
-// measurementWrapper(task1);
-measurementWrapper(task1a);
+measurementWrapper(task1);
+measurementWrapper(task2);
