f34ba117046b6302ede1a0d49ab52d43e73ae77a
Aliendreamer <dreamingman83@gmail.com>
2024-12-21 16:27:39 +0200

    day 20

--- adventofcode2024/day20/20.js
+++ adventofcode2024/day20/20.js
@@ -1,103 +1,117 @@
 import path from 'path';
-import { readInput } from '../helpers.mjs';
-const startTime = Date.now();
+import { readInput, getEndpoints, measurementWrapper, GridMovePatterns, isValid, isWall } from '../helpers.mjs';
 
-const dirs = [
-    [-1, 0],
-    [0, 1],
-    [1, 0],
-    [0, -1],
-];
-const dirs2 = [
+const GridMovePatterns2 = [
     [-2, 0],
     [0, 2],
     [2, 0],
     [0, -2],
-]; // Jump through a single wall
-const racetrack = new Map();
-const filepath = path.resolve(process.cwd(), 'adventofcode2024', 'day20', 'input.txt');
-const map = readInput(filepath)
-    .split('\n')
-    .map((x) => x.split(''));
-let dir = [0, 0]; // Initial state is not moving
-let answerp1 = 0;
-let answerp2 = 0;
-let pico = 0;
-let s, e, x, y;
+];
+const toKey = (r, c) => `${r},${c}`;
 
-// Find S and E, and initiate each map key
-for (let y = 0; y < map.length; y++) {
-    for (let x = 0; x < map[0].length; x++) {
-        if (map[y][x] === 'S') {
-            s = [y, x];
-            // At first I was lazily not storing x and y individually
-            // Thinking I could just parse the key itself when reading an entry
-            // While true, it slowed down this script about 50x
-            // So now we save x and y without being lazy
-            racetrack.set(`${y},${x}`, { picos: pico, x: x, y: y });
-            pico++;
+const task1 = () => {
+    const filepath = path.resolve(process.cwd(), 'adventofcode2024', 'day20', 'input.txt');
+    const map = readInput(filepath)
+        .split('\n')
+        .map((x) => x.split(''));
+    const { start, end } = getEndpoints(map, map.length, map[0].length, 'S', 'E');
+    const rows = map.length;
+    const cols = map[0].length;
+    const visited = new Map();
+    const heap = [];
+    let backTrack = [];
+    heap.push([start[0], start[1], [], 0]);
+    while (heap.length > 0) {
+        const [r, c, backpath, steps] = heap.shift();
+
+        const currentKey = toKey(r, c);
+        if (visited.has(currentKey)) {
+            continue;
         }
-        if (map[y][x] === 'E') e = [y, x];
+        visited.set(currentKey, { r, c, steps });
+
+        if (r === end[0] && c === end[1]) {
+            backTrack = [...backpath, `${r},${c}`];
+            break;
+        }
+
+        for (let index = 0; index < GridMovePatterns.length; index++) {
+            const element = GridMovePatterns[index];
+            const nr = r + element[0];
+            const nc = c + element[1];
+            if (isValid(nr, nc, rows, cols) && !isWall(map[nr][nc])) {
+                heap.push([nr, nc, [...backpath, currentKey], steps + 1]);
+            }
+        }
     }
-}
-// Set back y, x at the starting pos
-[y, x] = s;
 
-// Build the racetrack
-while (true) {
-    // Try every direction to find the next track
-    dirs.forEach(([dy, dx]) => {
-        const newY = y + dy;
-        const newX = x + dx;
-        // If wall, skip.
-        if (map[newY][newX] === '#') return;
-        // If 180-degree turn, skip
-        if (dir[0] * dy + dir[1] * dx === -1) return;
-        // We found the next track, set it as new pos
-        [y, x, dir] = [newY, newX, [dy, dx]];
-        // Save it in the racetrack, along with the picos to get there
-        racetrack.set(`${y},${x}`, { picos: pico, x: x, y: y });
-        pico++;
-    });
-    // Exit found
-    if (e[0] === y && e[1] === x) break;
-}
+    let answer1 = 0;
+    for (const step of backTrack) {
+        const [x, y] = step.split(',').map(Number);
+        GridMovePatterns2.forEach(([dx, dy]) => {
+            const newX = x + dx;
+            const newY = y + dy;
+            const cheatFrom = visited.get(step);
 
-// Part 1 - Jump over a single wall
-// This works without Array.from in the dev console, but we add it anyway for Node.js shenanigans
-const trackArr = Array.from(racetrack.entries());
-// For each track
-trackArr.forEach(([_, obj]) => {
-    // Check if a track exists 2 steps away
-    dirs2.forEach(([dy, dx]) => {
-        const newY = obj.y + dy;
-        const newX = obj.x + dx;
-        const cheatTo = racetrack.get(`${newY},${newX}`);
-        // If so and the diff is >= 100 + the cost of the cheat, increment answer
-        if (cheatTo && cheatTo.picos - obj.picos >= 100 + 2) answerp1++;
-    });
-});
-
-// Part 2 - Manhattan distance
-// No need to scan the last 100 values + minimal cost of cheat of 2, since it won't be a cheat worth 100 points +
-for (let i = 0; i < trackArr.length - (100 + 2); i++) {
-    // Extract values
-    const from = trackArr[i][1];
-    const [y, x, cheatFrom] = [from.y, from.x, from.picos];
-    // Evaluate all tracks AFTER the current one
-    for (let j = i + 1; j < trackArr.length; j++) {
-        // Extract values
-        const to = trackArr[j][1];
-        const [y2, x2, cheatTo] = [to.y, to.x, to.picos];
-        // Calc Manhattan distance between the tracks
-        const yDiff = Math.abs(y - y2);
-        const xDiff = Math.abs(x - x2);
-        const Manhattan = yDiff + xDiff;
-        // If Manhattan is 20 or less, and diff is >= 100 + cost of cheat. increment answer
-        if (Manhattan <= 20 && cheatTo - cheatFrom >= 100 + Manhattan) answerp2++;
+            const cheatTo = visited.get(`${newX},${newY}`);
+            // If so and the diff is >= 100 + the cost of the cheat, increment answer
+            if (cheatTo && cheatTo.steps - cheatFrom.steps >= 100 + 2) answer1++;
+        });
     }
-}
+    console.log(answer1);
+};
 
-console.log(`Answer Part 1: ${answerp1}`);
-console.log(`Answer Part 2: ${answerp2}`);
-console.log(`Solved in ${Date.now() - startTime} ms.`);
+const task2 = () => {
+    const filepath = path.resolve(process.cwd(), 'adventofcode2024', 'day20', 'input.txt');
+    const map = readInput(filepath)
+        .split('\n')
+        .map((x) => x.split(''));
+    const { start, end } = getEndpoints(map, map.length, map[0].length, 'S', 'E');
+    const rows = map.length;
+    const cols = map[0].length;
+    const visited = new Map();
+    const heap = [];
+    let backTrack = [];
+    heap.push([start[0], start[1], [], 0]);
+    while (heap.length > 0) {
+        const [r, c, backpath, steps] = heap.shift();
+
+        const currentKey = toKey(r, c);
+        if (visited.has(currentKey)) {
+            continue;
+        }
+        visited.set(currentKey, { r, c, steps });
+
+        if (r === end[0] && c === end[1]) {
+            backTrack = [...backpath, `${r},${c}`];
+            break;
+        }
+
+        for (let index = 0; index < GridMovePatterns.length; index++) {
+            const element = GridMovePatterns[index];
+            const nr = r + element[0];
+            const nc = c + element[1];
+            if (isValid(nr, nc, rows, cols) && !isWall(map[nr][nc])) {
+                heap.push([nr, nc, [...backpath, currentKey], steps + 1]);
+            }
+        }
+    }
+    // Part 2 - Manhattan distance
+    let answer = 0;
+    for (let i = 0; i < backTrack.length; i++) {
+        const stepsFrom = visited.get(backTrack[i]);
+        for (let j = i + 1; j < backTrack.length; j++) {
+            const stepsTo = visited.get(backTrack[j]);
+            // Calc Manhattan distance between the tracks
+            const xDiff = Math.abs(stepsFrom.r - stepsTo.r);
+            const yDiff = Math.abs(stepsFrom.c - stepsTo.c);
+            const Manhattan = xDiff + yDiff;
+            // If Manhattan is 20 or less, and diff is >= 100 + cost of cheat. increment answer
+            if (Manhattan <= 20 && stepsTo.steps - stepsFrom.steps >= 100 + Manhattan) answer++;
+        }
+    }
+    console.log(answer);
+};
+
+measurementWrapper(task1);
+measurementWrapper(task2);
