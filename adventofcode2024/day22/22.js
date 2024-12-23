985ed28aff233d9a04e586f5f95ad9b9a0519e19
Teodor Stefanov <teodor.g.stefanov@a1.bg>
2024-12-23 09:05:04 +0200

    clean up

--- adventofcode2024/day22/22.js
+++ adventofcode2024/day22/22.js
@@ -19,19 +19,55 @@ const calculate = (secret) => {
     return secret;
 };
 
-const run = (number, calculated) => {
+const run = (number, calculated, arr) => {
     let currentSecretNumber = number;
 
     for (let index = 0; index < 2000; index++) {
         if (calculated.has(currentSecretNumber)) {
             currentSecretNumber = calculated.get(currentSecretNumber);
+            arr.push(currentSecretNumber % 10);
             continue;
         }
         const nextSecretNumber = calculate(currentSecretNumber);
         calculated.set(currentSecretNumber, nextSecretNumber);
         currentSecretNumber = nextSecretNumber;
+        arr.push(currentSecretNumber % 10);
     }
     return currentSecretNumber;
+};
+
+const calculateBananas = (lastDigits, countBananas) => {
+    lastDigits.forEach((arr) => {
+        const seen = new Set();
+        // Keep track of seen patterns to evaluate them once per secret number
+        // Initialize the first pattern values
+        let [a, b, c, d, e] = [arr[0], arr[1], arr[2], arr[3], arr[4]];
+        // Convert into initial diffs
+        a = b - a;
+        b = c - b;
+        c = d - c;
+        d = e - d;
+        for (let i = 0; i < arr.length - 4; i++) {
+            // Avoid overly scanning the array, shift values instead
+            if (i) {
+                a = b;
+                b = c;
+                c = d;
+                d = arr[i + 4] - arr[i + 3];
+            }
+            // Create string pattern to use as key
+            const key = `${a},${b},${c},${d}`;
+            // Get bananas
+            const bananas = arr[i + 4];
+            // If it's the first time we see this key for the current secret number
+            if (!seen.has(key)) {
+                // Remember it
+                seen.add(key);
+                // Add bananas to the count for this key
+                countBananas.set(key, (countBananas.get(key) || 0) + bananas);
+            }
+        }
+    });
 };
 
 const task1 = () => {
@@ -40,10 +76,31 @@ const task1 = () => {
     const calculated = new Map();
     let monkeyNumbers = 0;
     for (const number of numbers) {
-        monkeyNumbers += run(number, calculated);
+        monkeyNumbers += run(number, calculated, []);
     }
 
     console.log(monkeyNumbers);
 };
 
+const task2 = () => {
+    const filepath = path.resolve(process.cwd(), 'adventofcode2024', 'day22', 'input.txt');
+    const numbers = readInput(filepath).split('\n').map(Number);
+    const calculated = new Map();
+    let monkeyNumbers = 0;
+    const countBananas = new Map();
+
+    const lastDigits = [];
+    for (const number of numbers) {
+        const temArr = [number % 10];
+        monkeyNumbers += run(number, calculated, temArr);
+        lastDigits.push(temArr);
+    }
+
+    calculateBananas(lastDigits, countBananas);
+    console.log(`Task 1 ${monkeyNumbers}`);
+
+    console.log(`Task 2 ${Math.max(...countBananas.values())}`);
+};
+
 measurementWrapper(task1);
+measurementWrapper(task2);
