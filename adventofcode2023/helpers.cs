b014eaf83a7e31bf9a471dbb3e31dffb8d76fbaa
Teodor Stefanov <teodor.g.stefanov@a1.bg>
2024-12-27 10:40:51 +0200

    made helpers

--- adventofcode2023/helpers.cs
+++ adventofcode2023/helpers.cs
@@ -21,5 +21,67 @@ namespace adventofcode2023
 			stopwatch.Stop();
 			Console.WriteLine($"Day  solution executed in: {stopwatch.ElapsedMilliseconds} ms");
 		}
+
+		private static readonly int[][] gridMovePatternsAndDiagonals =
+		[
+			new int[] {0, 1}, // Right
+            new int[] {0, -1}, // Left
+            [1, 0], // Down
+            new int[] {-1, 0}, // Up
+            new int[] {1, 1}, // Down-Right
+            new int[] {1, -1}, // Down-Left
+            new int[] {-1, 1}, // Up-Right
+            new int[] {-1, -1}, // Up-Left
+        ];
+
+		private static readonly int[][] gridMovePatterns =
+		[
+			new int[] {0, 1}, // Right
+            new int[] {1, 0}, // Down
+            new int[] {0, -1}, // Left
+            new int[] {-1, 0}, // Up
+        ];
+
+		public static int[][] GridMovePatternsAndDiagonals => gridMovePatternsAndDiagonals;
+		public static int[][] GridMovePatterns => gridMovePatterns;
+
+		public static bool IsValid(int x, int y, int rows, int cols) => x >= 0 && y >= 0 && x < rows && y < cols;
+
+		public static void DrawGrid(char[][] grid)
+		{
+			foreach (var row in grid)
+			{
+				Console.WriteLine(string.Join("", row));
+			}
+		}
+
+		public static bool IsWall(char position) => position == '#';
+
+		public static bool IsStart(char position) => position == 'S';
+		public static bool IsEnd(char position) => position == 'E';
+
+		public static (int[] startPoint, int[] endPoint) GetStartPoint(char[][] grid)
+		{
+			int[] startPoint, endPoint;
+			for (int i = 0; i < grid.Length; i++)
+			{
+				for (int j = 0; j < grid[0].Length; j++)
+				{
+					if (IsStart(grid[i][j]))
+					{
+						startPoint = [i, j];
+					}
+					if (IsEnd(grid[i][j]))
+					{
+						endPoint = [i, j];
+					}
+				}
+			}
+			return (startPoint, endPoint);
+		}
+
+		public static string ToKey(dynamic value1, dynamic value2) => $"{value1}_{value2}";
+
+		public static string ToKey(dynamic value1, dynamic value2, dynamic value3) => $"{value1}_{value2}_${value3}";
 	}
 }
